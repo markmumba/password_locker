7ddc2c2016aad23079529107fae80bc6bd1d8292
markmuba <markmumba01@gmail.com>
2019-07-21 13:28:34 +0300

    aplied search function and dispay function

--- py_test.py
+++ py_test.py
@@ -1,6 +1,6 @@
 import unittest
 import pyperclip
-from py_lock import personas,profiles
+from py_lock import personas, profiles
 
 
 class Testpersona(unittest.TestCase):
@@ -13,51 +13,67 @@ class Testpersona(unittest.TestCase):
 
     def test_save_persona(self):
         self.new_persona.save_persona()
-        self.assertEqual(len(profiles.profiles_list),1)
+        self.assertEqual(len(profiles.profiles_list), 1)
 
 
 class Testprofile(unittest.TestCase):
-    
+
     def SetUp(self):
-        self.new_profile = profiles("instagram","markian","Mortyka56//")
+        self.new_profile = profiles("instagram", "markian", "Mortyka56//")
+
     def test_int(self):
-        self.assertEqual(self.new_profile.app,"instagram")
-        self.assertEqual(self.new_profile.username,"markian")
-        self.assertEqual(self.new_profile.password,"Mortyka56//")
-        
+        self.assertEqual(self.new_profile.app, "instagram")
+        self.assertEqual(self.new_profile.username, "markian")
+        self.assertEqual(self.new_profile.password, "Mortyka56//")
+
     def save_profile(self):
-           self.new_profile.save_profile()
-           self.assertEqual(len(profiles.profiles_list),1)
-           
+        self.new_profile.save_profile()
+        self.assertEqual(len(profiles.profiles_list), 1)
+
     def tearDown(self):
-        profiles.profiles_list=[]
-        
+        profiles.profiles_list = []
+
     def test_save_mutiple_profiles(self):
         self.new_profile.save_profile()
         test_profile = profiles(
-            "instagram","markian","Mortyka56//"
+            "instagram", "markian", "Mortyka56//"
         )
         test_profile.save_profile()
-        self.assertEqual(len(profiles.profiles_list),2)
-        
-        
+        self.assertEqual(len(profiles.profiles_list), 2)
+
     def test_delete_profile(self):
         self.new_profile.save_profile()
         test_profile = profiles(
-            "instagram","markian","Mortyka56//"
+            "instagram", "markian", "Mortyka56//"
         )
         test_profile.delete_profile()
-        self.assertEqual(len(profiles.profiles_list),1)
-        
+        self.assertEqual(len(profiles.profiles_list), 1)
+
     def test_search_profile(self):
         self.new_profile.save_profile()
         test_profile = profiles(
-            "instagram","markian","Mortyka56//"
+            "instagram", "markian", "Mortyka56//"
         )
         test_profile.save_profile()
-        
+
         found_profile = profiles.search_profile("instagram")
+
+        self.assertEqual(found_profile.app, test_profile.app)
+
+    def test_profile_exist(self):
+        self.new_profile.save_profile()
+        test_profile = profiles(
+            "instagram", "markian", "Mortyka56//"
+        )
+        test_profile.save_profile()
+
+        profile_exists = profiles.profile_exist("instagram")
+
+        self.assertTrue(profile_exists)
+
+    def test_display_profile(self):
         
         
+        self.assertEqual(profiles.display_profile(),profiles.profiles_list)
         
-      
+        
